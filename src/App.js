d0131bb5f9c67734ba235124dfa379f7502f5194
iqbalyarkhan <iqbal.khan@utdallas.edu>
2021-07-30 16:32:14 -0500

    Updates react essentials

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Header(props){
+  return(
+      <header>
+        <h1>{props.name}'s Kitchen</h1>
+      </header>
+  );
+}
+
+function Main(props){
+    return(
+      <section>
+          We have every type of dish!
+          <ul style={{textAlign : "left"}}>
+              {props.dishes.map(
+                  (dish) => <li key={dish.toString()}>{dish}</li>
+              )}
+          </ul>
+      </section>
+    );
+}
+
+function Footer(props){
+    return(
+        <p>
+            {props.year} CopyRights reserved
+        </p>
+    );
+}
+
+const dishes = [
+    "Mac and cheese",
+    "Salmon",
+    "BBQ Chicken",
+    "Rice"
+];
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Header name="Iqbal"/>
+        <Main dishes={dishes}/>
+      <Footer year={new Date().getFullYear().toString()}/>
     </div>
   );
 }

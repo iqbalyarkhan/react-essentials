431838bd45f6d3827d71889535a20c02cef50b1c
iqbalyarkhan <iqbal.khan@utdallas.edu>
2021-08-01 18:34:54 -0500

    Updates react essentials

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
 
-//https://api.github.com/users/iqbalyarkhan
+import {
+    Home,
+    About,
+    Events,
+    Contact,
+    Whoops404
+} from "./pages"
 
-
-function App(props) {
+function App() {
     return(
-        <h1>Hello react testing library!</h1>
+        <div>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/events" element={<Events/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="*" element={<Whoops404/>}/>
+            </Routes>
+        </div>
     )
 }
 
